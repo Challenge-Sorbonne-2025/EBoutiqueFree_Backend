60c259ea831ccf94ddf2a5c354fdeeb3c7c217f9
etienne <etiennesene.ds@gmail.com>
2025-05-04 21:10:37 +0200

    modif entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 set -e  # Stop on error
 
 echo "📦 Collecting static files..."
-python manage.py collectstatic --noinput
+python EBoutique_API/manage.py collectstatic --noinput
 
 echo "🛠 Running migrations..."
-python manage.py migrate --noinput
+python EBoutique_API/manage.py migrate --noinput
 
 echo "🚀 Starting development server on 0.0.0.0:8000 ..."
-exec python manage.py runserver 0.0.0.0:8000
+exec python EBoutique_API/manage.py runserver 0.0.0.0:8000
