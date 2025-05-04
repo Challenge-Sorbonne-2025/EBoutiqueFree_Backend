a1019c85f4325808db4979a91bc58c2d06b15675
etienne <etiennesene.ds@gmail.com>
2025-05-05 00:38:35 +0200

    fix: port 7000

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ python EBoutique_API/manage.py collectstatic --noinput
 echo "🛠 Running migrations..."
 python EBoutique_API/manage.py migrate --noinput
 
-echo "🚀 Starting development server on 0.0.0.0:8000 ..."
-exec python EBoutique_API/manage.py runserver 0.0.0.0:8000
+echo "🚀 Starting development server on 0.0.0.0:7000 ..."
+exec python EBoutique_API/manage.py runserver 0.0.0.0:7000
