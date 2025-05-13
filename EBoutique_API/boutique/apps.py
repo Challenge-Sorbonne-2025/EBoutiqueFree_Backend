1366c5b28150f3f06b5119fc2eaefda5c5fd54c8
Sow-Sow9 <m2.sow7@gmail.com>
2025-05-13 11:44:54 +0200

    j'ai remplacé 'apps.boutique' par 'boutique', apps n'étant pas un dossier pour éviter un bug

--- EBoutique_API/boutique/apps.py
+++ EBoutique_API/boutique/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class BoutiqueConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.boutique'
+    name = 'boutique'
     verbose_name = "Gestion des boutiques Free"
     def ready(self):
         import apps.boutique.signals  # Active les signaux à l'initialisation
