fecba024f37f5f608af0d7ecd3e795bd870a8f05
adiouk <dioukgueye@gmail.com>
2025-06-04 11:44:42 +0200

    update settings file ...

--- EBoutique_API/eboutique_config/settings.py
+++ EBoutique_API/eboutique_config/settings.py
@@ -164,6 +164,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Votre frontend Vite
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
