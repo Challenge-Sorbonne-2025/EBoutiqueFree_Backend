4dff3c3b81e6ba0ffde6bb0c8e44f736cfcdac0a
Etienne <etiennesene.ds@gmail.com>
2025-06-11 13:51:24 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,10 @@
 
 set -e  # Stop on error
 
-pwd
 cd EBoutique_API
-pwd
-
-#echo "📦 Collecting static files..."
-#python3 manage.py collectstatic --noinput
 
 echo "🛠 Running migrations..."
-python3 manage.py migrate #--noinput
+python3 manage.py migrate
 
-echo "🚀 Starting development server on 127.0.0.1:7000 ..."
-exec python3 manage.py runserver 127.0.0.1:7000
+echo "🚀 Starting development server on 0.0.0.0:${PORT:-9000} ..."
+exec python3 manage.py runserver 0.0.0.0:${PORT:-9000}
