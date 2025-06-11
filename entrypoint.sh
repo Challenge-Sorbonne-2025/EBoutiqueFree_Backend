6b50c2961b144d963743b5f8483a2442d3fb3eb0
Etienne <etiennesene.ds@gmail.com>
2025-06-11 09:08:34 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ cd EBoutique_API
 echo "🛠 Running migrations..."
 python3 manage.py migrate
 
-echo "🚀 Starting development server on 0.0.0.0:${PORT:-5000} ..."
-exec python3 manage.py runserver 0.0.0.0:${PORT:-5000}
+echo "🚀 Starting development server on 0.0.0.0:${PORT:-8000} ..."
+exec python3 manage.py runserver 0.0.0.0:${PORT:-8000}
